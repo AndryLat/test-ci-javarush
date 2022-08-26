1f52ef5229b0d98a1f56a342d4a8f78f11768d5b
Andrii Latyshev <andrii.latyshev@gmail.com>
2022-08-26 19:43:27 +0300

    App skeleton

---
+++ src/test/java/dev/andrylat/test_ci/MainTest.java
@@ -0,0 +1,12 @@
+package dev.andrylat.test_ci;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
